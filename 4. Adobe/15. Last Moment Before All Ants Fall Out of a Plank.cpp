82db3476aeae180cdeef1369de63eafa2b4897c3
Jash Hinger <jashhinger.2020cs@technonjr.org>
2024-01-21 09:29:04 +0530

    added code for Adobe 13-15

---
+++ 4. Adobe/15. Last Moment Before All Ants Fall Out of a Plank.cpp
@@ -0,0 +1,10 @@
+// Link : https://leetcode.com/problems/last-moment-before-all-ants-fall-out-of-a-plank/description/
+
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int l=right.empty()?0:n-*min_element(right.begin(), right.end());
+        int r=left.empty()?0:*max_element(left.begin(), left.end());
+        return max(l, r);
+    }
+};
